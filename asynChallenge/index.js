434a04279e2a145e6f296eaa387b13c95af386cb
Abhay <abhaybisht9412@gmail.com>
2022-07-13 12:08:07 +0530

    async hall commit

---
+++ asynChallenge/index.js
@@ -0,0 +1,12 @@
+// cred ops using asynchrounous methodologies
+
+const fs = require("fs");
+
+// fs.writeFile("bio.txt","this is old content for async ",(err) => {
+//     console.log("file created");
+// })
+
+// fs.appendFile("asynChallenge/bio.txt","this is the new content here async",(err) => {
+//     console.log("file appended success");
+// })
+
