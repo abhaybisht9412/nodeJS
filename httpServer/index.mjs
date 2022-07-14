56bfbe50e23a216ab1db93a4fea63eaec582f113
Abhay <abhaybisht9412@gmail.com>
2022-07-14 14:08:49 +0530

    routing done

--- httpServer/index.mjs
+++ httpServer/index.mjs
@@ -1,13 +1,34 @@
 //importing from http module
+const log = console.log;
 import http from "http" ;
 //creating server
 const server = http.createServer((req,res) => {
     //request object used to get info about current http request
     //eg. url,request,header and data.
-    res.end("hello from the other side!");
+    //reqeust url afftr localhost:8000/contact                      
+    log(req.url);
+    if(req.url == "/"){
+        res.end("hello from the home side!");
+    }else if(req.url == "/about"){
+        res.end("hello from the about side!");
+    }else if(req.url == "/contact"){
+        res.end("hello from the contact side!");
+    }else{
+        //it will still show the status code as 200 which is incorrect it shoudle be in the range 400-500 in the network panel in inspect
+        res.writeHead(404,{"Content-type": "text/html"}); //now status 404 \/ and content type html
+        res.end("<h1> Error 404 ! not found :( </h1>");
+    }
 });
 
 server.listen(8000, "127.0.0.1", () => {
     console.log("listening to port no. 8000") ;
 });
 
+
+
+
+// Informational responses (100–199)
+// Successful responses (200–299)
+// Redirection messages (300–399)
+// Client error responses (400–499)
+// Server error responses (500–599)
