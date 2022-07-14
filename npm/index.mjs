89a8f63d8b369387e6040ee16b62cc45ad1f7c2a
Abhay <abhaybisht9412@gmail.com>
2022-07-14 11:16:23 +0530

    npm initial

--- npm/index.mjs
+++ npm/index.mjs
@@ -1,7 +1,12 @@
 import chalk from "chalk";
-
+import validator from 'validator';
 const log = console.log;
- log(chalk.blue('hello'));
+ //log(chalk.blue('hello'));
 
+  const res = validator.isEmail("abhaybisht9412@gmail.com");
+  log( res ? chalk.green.inverse(res) : chalk.red.inverse(res));
+
+//  const res = validator.isEmail("abhaybisht9412@gmailcom");
+//  log( res ? chalk.green.inverse(res) : chalk.red.inverse(res));
 
 
