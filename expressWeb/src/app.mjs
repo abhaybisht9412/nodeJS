6fd94936363ee55ef53a4abc309b392732044bb1
Abhay <abhaybisht9412@gmail.com>
2022-07-27 13:41:41 +0530

    commit

--- expressWeb/src/app.mjs
+++ expressWeb/src/app.mjs
@@ -1,24 +1,40 @@
 import express from 'express';
+import hbs from 'hbs' ;
 
 const app = express() ;
 const port = process.env.PORT || 8000;
 
+//using view engine handlebars
+app.set("view engine", 'hbs') ;
+//after renaming views and creating partials
+app.set('views','../src/templates/views') ;
+//using public files 
 app.use(express.static('../public'));
+//setting partials 
+hbs.registerPartials('../src/templates/views') ;
 
+
+
+//routing 
+//render instead of send
 app.get('/', (req,res) => {
-    res.send("Welcome to Home ") ;
+    res.render('index') ;
 })
 
+// app.get('/about', (req,res) => {
+//     res.send("Welcome to about ") ;
+// })
 app.get('/about', (req,res) => {
-    res.send("Welcome to about ") ;
+    res.render('about.hbs') ;
 })
+
 
 app.get('/weather', (req,res) => {
-    res.send("Welcome to app ") ;
+    res.render("Welcome to app ") ;
 })
 
 app.get('*', (req,res) => {
-    res.send(" Error 404 not found :( ") ;
+    res.render(" Error 404 not found :( ") ;
 })
 
 app.listen(port, () => {
