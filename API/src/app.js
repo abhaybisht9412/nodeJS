031a966f8cab4994c6caa12df799184b3b7b0b29
Abhay <abhaybisht9412@gmail.com>
2022-10-20 20:00:38 +0530

    final commit maybe

--- API/src/app.js
+++ API/src/app.js
@@ -5,7 +5,7 @@ const myModel = require("../src/models/people")
 const app = express();
 const port = 8000;
 
-app.use(express.json())
+app.use(express.json());
 
 // app.get("/" , (req  , res) => {
 //     res.send("Hello");
---
+++ weather UI app/src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const port = process.
+
+app.get("",(req,res) => {
+    res.send("hello");
+})
